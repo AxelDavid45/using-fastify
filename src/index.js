8c3461d0fa4fc9f3355546a87d126572352f384d
Axel Espinosa <axeldavid45@gmail.com>
2021-02-26 18:51:08 -0600

    feat: first route handler

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const fastify = require('fastify')({
+  logger: true
+})
+
+
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world'})
+})
+
+fastify.listen(3000, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`Server listening on ${address}`)
+})
